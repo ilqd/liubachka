49eaff3f96cd6102013447308569c613a8c43157
ikudimov <ikudimoff@gmail.com>
2017-08-11 22:03:43 +0300

    Initial commit

---
+++ src/main/java/ru/splashcourse/liuback/LiubackApplication.java
@@ -0,0 +1,12 @@
+package ru.splashcourse.liuback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiubackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiubackApplication.class, args);
+	}
+}
