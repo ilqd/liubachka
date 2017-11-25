4fe1aeb1d89b5cc0d2559a207c56129d7a727449
ikudimov <ikudimoff@gmail.com>
2017-11-26 01:00:02 +0300

    ап версий фронта

--- src/main/frontend/app/components/admin/stuff/CardCreator.jsx
+++ src/main/frontend/app/components/admin/stuff/CardCreator.jsx
@@ -11,7 +11,7 @@ export default class CardCreator extends React.Component {
         this.setLabel = this.setLabel.bind(this);
         this.setLinesCount = this.setLinesCount.bind(this);
         const delimeter = '  ';
-        const text = 'Our  gorgeous  teacher  knows  that  I  can\'t  say  no  and  made  me  do  this  application  of  text  splitting  and  creating  card  for  the activity  game';
+        const text = 'I\'m  beign held  in basement  and  was forced  to do  this  text splitting  and  creating  cards  for  the activity  game  application  If  you  see  this  message  call  911  please';
         const label = 'I\'m just a little harmless label, dont touch me please  :(';
         const linesCount = 10;
         this.state = {delimeter, linesCount, label, text, data: this.createData(text, delimeter, label, linesCount)};
--- src/main/frontend/webpack.config.js
+++ src/main/frontend/webpack.config.js
@@ -1,116 +1,95 @@
-'use strict';
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const merge = require('webpack-merge');
+const CompressionPlugin = require('compression-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-var path = require('path');
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var StatsPlugin = require('stats-webpack-plugin');
+const TARGET = process.env.npm_lifecycle_event;
 
 const PATHS = {
-		source : path.join(__dirname, 'app'),
-		output : path.join(__dirname, '../../../target/classes/static')
-	};
+    source: path.join(__dirname, 'app'),
+    output: path.join(__dirname, '../../../target/classes/static')
+};
 
-module.exports = {
-    // The entry file. All your app roots from here.
-	entry : [ 'babel-polyfill', PATHS.source ],
-    // Where you want the output to go
-    output : {
-		path : PATHS.output,
-		publicPath : '/',
-		filename : 'bundle.js'
-	},
-    resolve : {
-      extensions : [ '', '.js', '.jsx' ],
-      root: [
-        path.resolve('./app')
-      ]
+const prod = ({
+    plugins: [
+        new UglifyJSPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+    ]
+});
+
+const common = ({
+    entry: [ 'babel-polyfill', PATHS.source ],
+    devtool: 'source-map',
+    output: {
+        path: PATHS.output,
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx' ],
+        modules: [
+            path.join(__dirname, 'app'),
+            'node_modules'
+        ],
     },
     plugins: [
-
-        // handles creating an index.html file and injecting assets. necessary because assets
-        // change name because the hash part changes. We want hash name changes to bust cache
-        // on client browsers.
+        new webpack.optimize.ModuleConcatenationPlugin(),
+        new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /de|en|ru/),
+        new CompressionPlugin({
+            asset: '[path].gz[query]',
+            algorithm: 'gzip',
+            test: /\.jsx$|\.js$|\.css$|\.html$/,
+            minRatio: 0.9
+        }),
+// handles creating an index.html file and injecting assets. necessary because assets
+// change name because the hash part changes. We want hash name changes to bust cache
+// on client browsers.
         new HtmlWebpackPlugin({
             template: 'app/index.tpl.html',
             inject: 'body',
             filename: 'index.html'
-        }),
-        // extracts the css from the js files and puts them on a separate .css file. this is for
-        // performance and is used in prod environments. Styles load faster on their own .css
-        // file as they dont have to wait for the JS to load.
-        new ExtractTextPlugin('[name]-[hash].min.css'),
-
-        // plugin for passing in data to the js, like what NODE_ENV we are in.
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('development')
         })
     ],
-
-    // ESLint options
-    eslint: {
-        configFile: '.eslintrc',
-        failOnWarning: false,
-        failOnError: true
-    },
-
     module: {
-        rules: [
-          {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: 'babel-loader',
+        }, {
+            test: /\.less$/,
+            use: [{
+                loader: 'style-loader' // creates style nodes from JS strings
+            }, {
+                loader: 'css-loader' // translates CSS into CommonJS
+            }, {
+                loader: 'less-loader' // compiles Less to CSS
+            }]
+        }, {
             test: /\.css$/,
             use: [ 'style-loader', 'css-loader' ]
-          }
-        ],
-        preLoaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'eslint'
-            }
-        ],
-        loaders: [
-            {
-                test: /\.js?$/,
-                exclude: /node_modules/,
-                loader: 'babel'
-            },
-            {
-              test: /.jsx?$/,
-              loader: 'babel-loader',
-              exclude: /node_modules/,
-              query: {
-                presets: ['es2015', 'react']
-              }
-            },
-            {
-               test: /\.(jpe?g|png|gif|svg)$/i,
-               loaders: [
-                 'file?hash=sha512&digest=hex&name=[hash].[ext]',
-                 'image-webpack-loader?{gifsicle: {interlaced: true}, optipng: {optimizationLevel: 7}, pngquant:{quality: "65-90", speed: 4}, mozjpeg: {quality: 65}}'
-               ]
-             },
-            {
-                test: /\.json?$/,
-                loader: 'json'
-            },
-            {
-                test: /\.scss$/,
-                loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]!sass'
-            },
-            {
-                test: /\.css$/,
-                loader: 'style!css'
-            },
-            {
-                test:/\.less$/,
-                exclude:'/node_modules',
-                loader:"style!css!less"
-            },
-            { test: /\.woff(2)?(\?[a-z0-9#=&.]+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-            { test: /\.(ttf|eot|svg)(\?[a-z0-9#=&.]+)?$/, loader: 'file' }
-        ]
-    },
-    postcss: [
-        require('autoprefixer')
-    ]
-};
+        }, {
+            test: /\.gif$/,
+            use: 'url-loader?mimetype=image/png'
+        }, {
+            test: /\.woff(2)?(\?v=[0-9].[0-9].[0-9])?$/,
+            use: 'url-loader?mimetype=application/font-woff'
+        }, {
+            test: /\.(ttf|eot|svg|png)(\?v=[0-9].[0-9].[0-9])?$/,
+            use: 'file-loader?name=[name].[ext]'
+        }, {
+            test: /\.json?$/,
+            use: 'json'
+        } ]}
+});
+
+if (TARGET === 'build') {
+    module.exports = merge(common, prod);
+} else {
+    module.exports = common;
+}
