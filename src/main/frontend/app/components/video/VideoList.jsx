fd6d8c7f716dfc5031eecec2537097b974b66ee6
ikudimov <ikudimoff@gmail.com>
2017-12-23 01:47:47 +0300

    стили видосиков

--- src/main/frontend/app/components/video/VideoList.jsx
+++ src/main/frontend/app/components/video/VideoList.jsx
@@ -17,7 +17,6 @@ class VideoBlock extends React.Component {
         this.openPlayer = this.openPlayer.bind(this);
     }
     openPlayer() {
-        window.scrollTo(0, 0);
         this.props.openPlayer(this.props.elem);
     }
     render() {
@@ -26,8 +25,10 @@ class VideoBlock extends React.Component {
               <img src={`https://img.youtube.com/vi/${this.props.elem.get('youtubeId')}/0.jpg`}/>
               <div className="btn btn-link video-play"><Glyphicon onClick={this.openPlayer} glyph="play"/></div>
             </div>
-            <div className="video-title">{this.props.elem.get('name')}</div>
-            <div className="video-author">{this.props.elem.get('creatorName')}</div>
+            <div className={`video-info ${this.props.listMode && 'video-info-list-mode'}`}>
+              <div className="video-title">{this.props.elem.get('name')}</div>
+              <div className="video-author">{this.props.elem.get('creatorName')}</div>
+            </div>
           </div>
         );
     }
@@ -35,29 +36,85 @@ class VideoBlock extends React.Component {
 VideoBlock.propTypes = {
     elem: React.PropTypes.object,
     openPlayer: React.PropTypes.func,
+    listMode: React.PropTypes.bool,
 };
+
+//eslint-disable-next-line
+class VideoPlayer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.onReady = this.onReady.bind(this);
+        this.opts = {
+            playerVars: {
+                height: '360',
+                width: '640',
+                rel: 0,
+                controls: 2,
+                modestbranding: 1,
+            }
+        };
+    }
+    componentDidMount() {
+        this.scroll();
+    }
+    onReady(event) {
+        event.target.pauseVideo();
+    }
+    scroll() {
+        setTimeout(()=> {
+            const elem = $('.video-player-container');
+            if (elem.length > 0) {
+                $('.video-player-container')[0].scrollIntoView();
+            }
+        }, 200);
+    }
+    render() {
+        const stubArray = [];
+        for (let i = 0; i < 10; i++) {
+            stubArray.push(i);
+        }
+        this.scroll();
+        return (<div>
+        <Row className="video-player">
+          <Col xs={12}>
+            <YouTube
+              videoId={this.props.videoId}
+              opts={this.opts}
+              onReady={this.onReady}
+            />
+            </Col>
+          </Row>
+          <Row>
+        {stubArray.map(i=><Col xs={11} xsOffset={1} key={i}>Коммент {i}</Col>)}
+          </Row>
+        </div>
+        );
+    }
+}
+VideoPlayer.propTypes = {
+    videoId: React.PropTypes.String,
+};
+
+
 //eslint-disable-next-line
 class VideoList extends React.Component {
     constructor(props) {
         super(props);
         this.openPlayer = this.openPlayer.bind(this);
         this.closePlayer = this.closePlayer.bind(this);
-        this.onReady = this.onReady.bind(this);
         this.showList = this.showList.bind(this);
         this.initWheel = this.initWheel.bind(this);
-        this.state = {showPlayer: false, showList: false};
+        this.state = {showPlayer: false, showList: false, savedYScroll: 0};
     }
     componentWillMount() {
         this.props.loadVideos();
     }
     openPlayer(elem) {
-        this.setState({showPlayer: true, videoId: elem.get('youtubeId')});
+        this.setState({savedYScroll: window.pageYOffset, showPlayer: true, videoId: elem.get('youtubeId')});
     }
     closePlayer() {
         this.setState({showPlayer: false});
-    }
-    onReady(event) {
-        event.target.pauseVideo();
+        window.scrollTo(0, this.state.savedYScroll);
     }
     showList() {
         this.setState({showList: !this.state.showList});
@@ -72,8 +129,10 @@ class VideoList extends React.Component {
             elem.bind('mousewheel', e=> {
                 if (e.originalEvent.wheelDelta / 120 > 0) { // up
                     that.refs.slick.slickPrev();
+                    e.preventDefault();
                 }else{// down
                     that.refs.slick.slickNext();
+                    e.preventDefault();
                 }
             });
             this.wheelInited = true;
@@ -85,28 +144,10 @@ class VideoList extends React.Component {
         const list = this.props.data.map((e, idx)=><div key={idx} className="video-block"><VideoBlock elem={e}  openPlayer={this.openPlayer}/></div>);
 
 
-        const opts = {
-            playerVars: {
-                height: '360',
-                width: '640',
-            }
-        };
-        const player = (<Row className="video-player">
-        <Col xs={12}>
-          <YouTube
-            videoId={this.state.videoId}
-            opts={opts}
-            onReady={this.onReady}
-          />
-          </Col>
-        </Row>);
         this.initWheel();
 
         return (<div className="video-page">
           <Row >
-          {show && <Col xs={12}>
-          {player}
-          </Col>}
           <Col xs={12}>
           <Slider className="video-carousel" ref="slick" arrows swipeToSlide responsive={ [
              { breakpoint: 768, settings: { slidesToShow: 3 } },
@@ -119,9 +160,16 @@ class VideoList extends React.Component {
           </Col>
           </Row>
           {this.state.showList && <Row className="video-row">
-              {this.props.data.map((e, idx)=><Col key={idx} xs={12} sm={4} md={3} lg={2} className="video-block"><VideoBlock show={show} openPlayer={this.openPlayer} elem={e}/></Col>)}
-            </Row>
-          }
+              {this.props.data.map((e, idx)=><Col key={idx} xs={12} sm={4} md={3} lg={2} className="video-block">
+              <VideoBlock show={show} openPlayer={this.openPlayer} elem={e} listMode/>
+              </Col>)}
+          </Row>  }
+          {show && <Row>
+            <Col xs={12} className="video-player-container">
+              <VideoPlayer videoId={this.state.videoId}/>
+            </Col>
+            <Button bsStyle="link" onClick={this.closePlayer}>Уродливая кнопка закрытия плеера</Button>
+          </Row>}
         </div>);
     }
 }
